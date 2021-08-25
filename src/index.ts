1836e880d2bbf8fc1018ff586696c0b4a22dff21
username40 <mr.v.e.romantsov@gmail.com>
2021-08-25 21:19:28 +0300

    render google map

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
+/*
 import {User} from './User'
 import {Company} from './Company'
+*/
 
-const user = new User()
-console.log(user)
-
-const company = new Company()
-console.log(company)
+new google.maps.Map(document.getElementById('map'), {zoom: 5, center: {lat: 0, lng: 0}})
