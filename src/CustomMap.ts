d3f4cfbe30716429bb4fa034a52addf24f344a1a
username40 <mr.v.e.romantsov@gmail.com>
2021-08-26 16:18:50 +0300

    render marker on the map

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,7 +1,25 @@
+import {User} from './User'
+//import {Company} from './Company'
+
 export class CustomMap {
     private googleMap: google.maps.Map
 
     constructor(mapDivId: string) {
-        this.googleMap = new google.maps.Map(document.getElementById(mapDivId), {zoom: 5, center: {lat: 0, lng: 0}})
+        this.googleMap = new google.maps.Map(document.getElementById(mapDivId), {zoom: 1, center: {lat: 0, lng: 0}})
     }
+
+    addUserMarker(user:User): void {
+        new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: user.location.lat,
+                lng: user.location.lng,
+            }
+        })
+    }
+/*
+    addCompanyMarker(company: Company): void {
+
+    }*/
 }
+
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-/*
 import {User} from './User'
-import {Company} from './Company'
-*/
+//import {Company} from './Company'
 import {CustomMap} from "./CustomMap";
 
-new CustomMap('map')
+const user = new User()
+const customMap = new CustomMap('map')
+
+customMap.addUserMarker(user)
 
 
