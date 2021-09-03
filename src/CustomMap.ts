ed10b6e10e42b87bf88fa0b12e84e7b5baffa3ea
username40 <mr.v.e.romantsov@gmail.com>
2021-09-03 22:08:03 +0300

    little bit of refactoring

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,5 +1,9 @@
-import {User} from './User'
-import {Company} from './Company'
+interface Mappable {
+    location: {
+        lat: number;
+        lng: number
+    }
+}
 
 export class CustomMap {
     private googleMap: google.maps.Map
@@ -8,22 +12,12 @@ export class CustomMap {
         this.googleMap = new google.maps.Map(document.getElementById(mapDivId), {zoom: 1, center: {lat: 0, lng: 0}})
     }
 
-    addUserMarker(user:User): void {
+    addMarker(mappable: Mappable): void {
         new google.maps.Marker({
             map: this.googleMap,
             position: {
-                lat: user.location.lat,
-                lng: user.location.lng,
-            }
-        })
-    }
-
-    addCompanyMarker(company: Company): void {
-        new google.maps.Marker({
-            map: this.googleMap,
-            position: {
-                lat: company.location.lat,
-                lng: company.location.lng,
+                lat: mappable.location.lat,
+                lng: mappable.location.lng,
             }
         })
     }
